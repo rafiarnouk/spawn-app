9369fc2cec2e4aaa4131ff42fbd24b233ee6df96
Daggerpov <danielagapov1@gmail.com>
2025-02-21 20:37:48 -0800

    Tracking whole app component

--- spawn/src/main.jsx
+++ spawn/src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Analytics } from "@vercel/analytics/react"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Analytics>
+      <App />
+    </Analytics>
+  </StrictMode>
+);
