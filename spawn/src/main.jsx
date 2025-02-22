35d333d55fcd5e9bc57d741be0b358702b4f5ee8
Daggerpov <danielagapov1@gmail.com>
2025-02-21 20:43:53 -0800

    fixed component wrapping

--- spawn/src/main.jsx
+++ spawn/src/main.jsx
@@ -6,8 +6,7 @@ import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Analytics>
+    <Analytics/>
     <App />
-    </Analytics>
-  </StrictMode>,
+  </StrictMode>
 )
